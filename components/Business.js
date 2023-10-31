9338c9ff4ab50ce45180a60132c58d5e0200b2df
lucaskaspary2022 <lucaskaspary@gmail.com>
2023-10-31 17:25:26 -0400

    issue fixed

--- components/Business.js
+++ components/Business.js
@@ -7,7 +7,7 @@ function Business() {
         <section id='features' className='flex md:flex-row flex-col sm:py-16 py-6'>
             <div className='flex-1 flex justify-center items-start flex-col'>
                 <h2 className='font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full'>
-                    You do the business, <br className='sm:block hidden'/> we'll handle the money.
+                    You do the business, <br className='sm:block hidden'/> we&apos;ll handle the money.
                 </h2>
                 <p className='font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5'>
                     With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. 
